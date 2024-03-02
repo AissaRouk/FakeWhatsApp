d34d5309ba7c47a8cd1e1b6ae00fb3fa6d805fe9
AissaRouk <aissa.rouk@gmail.com>
2024-03-02 20:54:15 +0200

    Component that show the contact info

--- src/Components/ContactComponents.tsx
+++ src/Components/ContactComponents.tsx
@@ -1,7 +1,13 @@
 import {Contact} from '../Types/Contact';
 import {TouchableOpacity, Image, Text} from 'react-native';
 
-export default function ContactComponent(contact: Contact): React.JSX.Element {
+export default function ContactComponent({
+  contact,
+  navigation,
+}: {
+  contact: Contact;
+  navigation: any;
+}): React.JSX.Element {
   return (
     <TouchableOpacity
       style={{
@@ -11,7 +17,8 @@ export default function ContactComponent(contact: Contact): React.JSX.Element {
         borderBottomColor: 'grey',
         borderBottomWidth: 1,
         paddingLeft: 15,
-      }}>
+      }}
+      onPress={() => navigation.navigate('ContactScreen', {contact})}>
       <Image
         source={{uri: contact.picture}}
         style={{width: 50, height: 50, borderRadius: 25}} // Specify width and height
