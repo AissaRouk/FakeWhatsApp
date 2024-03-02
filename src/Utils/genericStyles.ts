a0df624e85a615632671b5fe095a5ab32833e498
AissaRouk <aissa.rouk@gmail.com>
2024-03-02 20:56:22 +0200

    generic styles

---
+++ src/Utils/genericStyles.ts
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+export const genericStyles = StyleSheet.create({
+  // Image styles
+  profileImageDimension: {
+    height: 50,
+    width: 50,
+  },
+  profileImageRadius: {
+    borderRadius: 25,
+  },
+
+  // Header styles
+  headerText: {
+    fontSize: 20,
+    fontWeight: '500',
+    color: 'white',
+  },
+});
