5913bf67c381af6fd5e0598305523c3ceab21dcd
AissaRouk <aissa.rouk@gmail.com>
2024-03-02 20:55:58 +0200

    Main Screen

--- src/Screens/MainScreen.tsx
+++ src/Screens/MainScreen.tsx
@@ -3,8 +3,9 @@ import ContactComponent from '../Components/ContactComponents';
 import {useState, useEffect} from 'react';
 import {Contact} from '../Types/Contact';
 import createRandomUser from '../Utils/createRandomUser';
+import HeaderComponent from '../Components/HeaderComponent';
 
-export default function MainScreen() {
+export default function MainScreen({navigation}: any) {
   const [contacts, setContacts] = useState<Contact[]>([]);
 
   useEffect(() => {
@@ -20,23 +21,17 @@ export default function MainScreen() {
 
   return (
     <>
-      <View
-        style={{
-          backgroundColor: 'green',
-          paddingVertical: 20,
-          paddingLeft: 10,
-        }}>
+      <HeaderComponent>
         <Text style={{fontSize: 20, fontWeight: '500', color: 'white'}}>
-          Messages{' '}
+          Messages
         </Text>
-      </View>
+      </HeaderComponent>
       <ScrollView>
         {contacts.map(item => (
           <ContactComponent
-            id={item.id}
             key={item.id}
-            name={item.name}
-            picture={item.picture}
+            contact={item}
+            navigation={navigation}
           />
         ))}
       </ScrollView>
