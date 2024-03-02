0d26dfb738ccf7f60214a5a0acc903988d9e785b
AissaRouk <aissa.rouk@gmail.com>
2024-03-02 20:52:48 +0200

    Navigation Container added

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MainScreen from './src/Screens/MainScreen';
+import ContactScreen from './src/Screens/ContactScreen';
+
+const Stack = createNativeStackNavigator();
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-      <MainScreen />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Main"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Main" component={MainScreen} />
+        <Stack.Screen name="ContactScreen" component={ContactScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
