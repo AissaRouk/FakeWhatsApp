1e97b0c1429a871fdc16660569a01cdf5feb01c0
AissaRouk <aissa.rouk@gmail.com>
2024-03-04 01:12:18 +0200

    Added Auth and AppStack

--- App.tsx
+++ App.tsx
@@ -1,22 +1,69 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, View, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MainScreen from './src/Screens/MainScreen';
 import ContactScreen from './src/Screens/ContactScreen';
+import LoginScreen from './src/Screens/LoginScreen';
+import SignUpScreen from './src/Screens/SignUpScreen';
+import auth from '@react-native-firebase/auth';
 
 const Stack = createNativeStackNavigator();
+const AuthStack = createNativeStackNavigator();
 
-function App(): React.JSX.Element {
+function LoadingScreen() {
+  return (
+    <View style={styles.loadingContainer}>
+      <ActivityIndicator size="large" color="green" />
+    </View>
+  );
+}
+
+function App() {
+  const [initializing, setInitializing] = useState<boolean>(true);
+  const [user, setUser] = useState<any>();
+
+  // Handle user state changes
+  function onAuthStateChanged(user: any) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return <LoadingScreen />;
+
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Main"
-        screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Main" component={MainScreen} />
-        <Stack.Screen name="ContactScreen" component={ContactScreen} />
-      </Stack.Navigator>
+      {user ? (
+        <Stack.Navigator
+          initialRouteName="Main"
+          screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Main" component={MainScreen} />
+          <Stack.Screen name="ContactScreen" component={ContactScreen} />
+        </Stack.Navigator>
+      ) : (
+        <AuthStack.Navigator
+          initialRouteName="Login"
+          screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+        </AuthStack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+});
 
 export default App;
